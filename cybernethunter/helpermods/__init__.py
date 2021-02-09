1e7c014e9e4e214a349b6ca4c44d121ffd433404
darkquasar <darkquasar7@gmail.com>
2021-02-09 17:29:17 +1000

    improving nb conversion

--- cybernethunter/helpermods/__init__.py
+++ cybernethunter/helpermods/__init__.py
@@ -2,4 +2,5 @@
 # 2018-2020 Diego Perez (@darkquassar / https://linkedin.com/in/diegope)
 
 from . import utils
+from . import notebook
 from . import transforms
--- cybernethunter/helpermods/notebook.py
+++ cybernethunter/helpermods/notebook.py
@@ -4,7 +4,7 @@
  NAME: notebook.py | version: 0.1
  CYBERNETHUNTER Version: 0.3
  AUTHOR: Diego Perez (@darkquasar) - 2020
- DESCRIPTION: Script to facilitate the conversion of YAML playbooks to iPython Notebooks
+ DESCRIPTION: Script to facilitate the conversion of YAML playbooks to iPython Notebooks and viceversa
     
  Updates: 
         v0.1 - 05-10-2020 - Created script.
@@ -22,7 +22,6 @@ import re
 import sys
 import yaml
 from pathlib import Path
-from tomark import Tomark
 
 from cybernethunter.helpermods import utils
 
@@ -39,10 +38,10 @@ class HelperMod:
             t = yaml.load(yf, Loader=yaml.FullLoader)
         return t
     
-    def create_notebook(self, yaml_playbook):
+    def create_notebook(self, yaml_playbook, output_nb_file=None):
         
         # Define notebook metadata
-        nb_meta:    {
+        nb_meta =   {
                         "metadata": {
                             "extensions": {
                             },
@@ -82,14 +81,37 @@ class HelperMod:
         nb['cells'].append(nbf.v4.new_markdown_cell(df_md_table))
         
         # Add Timestamp
-        df = pd.DataFrame([t['playbook_meta']['timestamp']]).T.rename(columns={0: ''})
+        df = pd.DataFrame([yaml_playbook['playbook_meta']['timestamp']]).T.rename(columns={0: ''})
         df_md_table = df.to_markdown()
         nb['cells'].append(nbf.v4.new_markdown_cell(df_md_table))
         
-        # Add Preamble
-        nb['cells'].append(nbf.v4.new_markdown_cell('# {}'.format(yaml_playbook['title'])))
-        
         # Add Generic Sections
-        
+        for section in yaml_playbook['generic_sections']:
+            cell_meta = {'playbook': section['metadata']}
+            if section['metadata']['type'] == 'markdown':
+                nb['cells'].append(nbf.v4.new_markdown_cell(section['content'], metadata=cell_meta))
+            elif section['metadata']['type'] == 'code':
+                nb['cells'].append(nbf.v4.new_code_cell(section['content'], metadata=cell_meta))
+
         # Add Analytics
+        for analytic in yaml_playbook['analytics']:
+            # Compose analysis description
+            cell_meta = {'playbook': analytic['metadata']}
+            an_desc = '## {}\r\n### Purpose\r\n{}\r\n### Method\r\n{}'.format(
+                                            analytic['name'], 
+                                            analytic['description']['purpose'], 
+                                            analytic['description']['method'])
+
+            nb['cells'].append(nbf.v4.new_markdown_cell(an_desc, metadata=cell_meta))
+            
+            for code in analytic['code']:
+                nb['cells'].append(nbf.v4.new_code_cell(code, metadata=cell_meta))
+            
+        nb_html = nbf.writes(nb, version=nbf.NO_CONVERT)
         
+        if output_nb_file != None:
+            out_file = Path(output_nb_file)
+            with open(out_file, 'w', encoding='utf-8') as nb_file:
+                nb_file.write(nb_html)
+        else:
+            return nb_html
